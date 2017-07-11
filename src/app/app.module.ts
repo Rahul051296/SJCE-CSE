10b86de820d919e667cb15059a4ddf983c467577
Rahul <rahulprabhakar0596@gmail.com>
2017-07-11 19:17:46 +0530

    First Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,58 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { LabComponent } from './lab/lab.component';
+import { FacultyComponent } from './faculty/faculty.component';
+import { StudentsComponent } from './students/students.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+import { RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutUsComponent,
+    LabComponent,
+    FacultyComponent,
+    StudentsComponent,
+    GalleryComponent,
+    ContactUsComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      {
+        path: '',
+        component: HomeComponent
+      },
+      {
+        path: 'about-us',
+        component: AboutUsComponent
+      },
+      {
+        path: 'contact-us',
+        component: ContactUsComponent
+      },
+      {
+        path: 'faculty',
+        component: FacultyComponent
+      },
+      {
+        path: 'gallery',
+        component: GalleryComponent
+      },
+      {
+        path: 'students',
+        component: StudentsComponent
+      },
+      {
+        path: 'laboratory',
+        component: LabComponent
+      }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
