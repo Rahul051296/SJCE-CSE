005b60766f17c8fc6428017cb23c2f94e7e763ba
Rahul <rahulprabhakar0596@gmail.com>
2017-07-12 11:32:21 +0530

    Added More Section

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -11,7 +11,35 @@ export class HomeComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    $(document).ready(function()
+  {
 
+      var scroll_start = 0;
+      var startchange = $('#title');
+      var offset = startchange.offset();
+
+$(document).scroll(function()
+  {
+      scroll_start = $(this).scrollTop();
+      if(scroll_start > offset.top)
+          {
+              $('.navbar-default').css('background-color', 'rgb(255, 255, 255)');
+              $('.navbar-default').css('box-shadow','0px 0px 8px 0px #010101');
+              $('.navbar-default').css('padding-bottom','10px');
+              $('.navbar-default ul>li>a').css('color','#000');
+              $('.navbar-brand').css('color','rgba(0,0,0,0.8)');
+              $('.fa-bars').css('color','#000');
+           }
+       else
+           {
+               $('.navbar-default').css('background-color', 'rgba(0, 0, 0, 0)');
+               $('.navbar-default ul>li>a').css('color','#fff');
+               $('.navbar-brand').css('color','#fff');
+               $('.navbar-default').css('box-shadow','0px 0px 0px 0px #000');
+               $('.fa-bars').css('color','#fff');
+          }
+  });
+  });
   }
 
 }
